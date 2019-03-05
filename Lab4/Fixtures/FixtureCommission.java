e6bd0cf5826391dbacbf26dd991ddca8f2d2c51d
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-05 10:04:20 -0600

    Update FixtureCommission.java

--- Lab4/Fixtures/FixtureCommission.java
+++ Lab4/Fixtures/FixtureCommission.java
@@ -5,16 +5,12 @@ public class FixtureCommission extends ColumnFixture {
 	public int locks, stocks, barrels;
 	
 	public double tstCalculateSales() {
-		return 100.00;	
+		//return 100.00;
+		return 10.00;
 	}
 	
 	public double tstCalculateCommission() {
 		return 10.00;
-	}
-	    
-
-	   
-	
-       
+	}      
     
 }
